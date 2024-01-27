bad6c648cb0b305346c2a44bb03066a7e827371a
PatheticApple <garynguyenquy1301@gmail.com>
2024-01-28 02:51:55 +1000

    Fix up Home Page and add one more questions type

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 
+import techQuestions from './tech.json'
+import mathQuestions from './math.json'
+import countryQuestions from './country.json'
 
 const questions = [
     {
@@ -53,21 +56,21 @@ export default function Home() {
                 <div className="col-12 col-xl-4 col-md-6">
                     <Link to="/question/1/tech" className="noTextDecoration"><div className="m-5 p-5 text-center text-light quizCards">
                         <h1>Tech Quiz</h1>
-                        <h2 className="display-6 py-5">{questions.length} Questions</h2>
+                        <h2 className="display-6 py-5">{techQuestions.length} Questions</h2>
                         <h3 className="py-2">Time</h3>
                     </div></Link>
                 </div>
                 <div className="col-12 col-xl-4 col-md-6">
                     <Link to="/question/1/math" className="noTextDecoration"><div className="m-5 p-5 text-center text-light quizCards">
                         <h1>Math Quiz</h1>
-                        <h2 className="display-6 py-5">{questions.length} Questions</h2>
+                        <h2 className="display-6 py-5">{mathQuestions.length} Questions</h2>
                         <h3 className="py-2">Time</h3>
                     </div></Link>
                 </div>
                 <div className="col-12 col-xl-4 col-md-6">
-                    <Link to="/question/1" className="noTextDecoration"><div className="m-5 p-5 text-center text-light quizCards">
-                        <h1>Trivia Quiz</h1>
-                        <h2 className="display-6 py-5">{questions.length} Questions</h2>
+                    <Link to="/question/1/country" className="noTextDecoration"><div className="m-5 p-5 text-center text-light quizCards">
+                        <h1>Country Quiz</h1>
+                        <h2 className="display-6 py-5">{countryQuestions.length} Questions</h2>
                         <h3 className="py-2">Time</h3>
                     </div></Link>
                 </div>
